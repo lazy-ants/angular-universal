30ad2c113d59470259fcf5d623bef10438688a69
Alexander G <alexgarmatenko@gmail.com>
2018-08-16 11:10:09 +0300

    Fix the issue with initialization NotFoundPage component many times #5

--- project/application/src/app/modules/core/services/http-response-status/http-response-status.service.ts
+++ project/application/src/app/modules/core/services/http-response-status/http-response-status.service.ts
@@ -2,50 +2,21 @@ import { Inject, Injectable, Optional } from '@angular/core';
 import { RESPONSE } from '@nguniversal/express-engine/tokens';
 
 import { CoreModule } from '../../core.module';
-import { TransferStateService } from '../transfer-state/transfer-state.service';
-
-interface PayloadDefinition {
-    payload: {
-        setStatusExecuted: boolean;
-    };
-}
 
 @Injectable({
     providedIn: CoreModule,
 })
 export class HttpResponseStatusService {
-    private payloadServerName = 'HttpResponseStatusServicePayload';
-
     constructor(
         @Optional()
         @Inject(RESPONSE)
-        private res: any,
-        private transferStateService: TransferStateService
+        private res: any
     ) {}
 
     public setStatus(code: number, message: string): void {
-        this.transferStateService
-            .savePayload(
-                () =>
-                    new Promise(resolve => {
-                        if (this.res) {
-                            this.res.statusCode = code;
-                            this.res.statusMessage = message;
-                        }
-
-                        const payload = { setStatusExecuted: true };
-
-                        resolve({ payload });
-                    }),
-                this.payloadServerName
-            )
-            .then(
-                (payload: PayloadDefinition) => {
-                    // payload received
-                },
-                error => {
-                    // error while receiving payload
-                }
-            );
+        if (this.res) {
+            this.res.statusCode = code;
+            this.res.statusMessage = message;
+        }
     }
 }
