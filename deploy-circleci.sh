fae19dd835f8fffb0b5527a1a163fb5aeae08e62
Alexander G <alexgarmatenko@gmail.com>
2018-09-06 00:08:28 +0300

    Extend CircleCI configuration to check the Nodejs server instance status #20
    - Updated deployment script
    - Extended CircleCI config with pm2 and full-icu packages installing (from nodejs Dockerfile)
    - Added the comments to the CircleCI parameters that need to be replaced in the case of changing deployment configuration

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,8 +1,4 @@
-echo 'Before checkout'
-git status
-git checkout -- .
-echo 'After checkout'
-git pull origin master
+#!/bin/bash
 docker-compose up -d --build
 docker exec -i angular-universal_nodejs npm install
 docker exec -i angular-universal_nodejs bash -c 'npm run build:ssr'
